c2a86820db0f33b82db8ef227aedf15b5725fd5a
Yevhen Piven <evgeniy13.91a@gmail.com>
2023-06-28 23:38:07 +0200

    make program who show only prime numbers

--- src/ua/com/yevhenpiven/Main.java
+++ src/ua/com/yevhenpiven/Main.java
@@ -1,27 +1,22 @@
 package ua.com.yevhenpiven;
 
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		// method sorted array from small to large
-		for (int i : buble_sort(new int[] { 12, -45, 0, 145, -85, -12, 14, -2859 }))
-			System.out.println(i);
-
-	}
-
-	public static int[] buble_sort(int[] arr) {
-
-		for (int i = 0; i < arr.length; i++) {
-			int a = 0;
-			for (int j = 0; j < arr.length - 1; j++) {
-
-				if (arr[j] > arr[j + 1]) {
-					a = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = a;
-				}
-			}
+		
+	for(int i=2;i<=100;i++) {
+		boolean isTrue=true;
+		for(int j=2;j<i;j++) {
+			if(i%j==0)
+			isTrue=false;
+			break;
 		}
-		return arr;
+		if (isTrue)
+			System.out.println(i);
+	}	
+	
 	}
+	
 }
