fa6921f52f607ecb76ca02cc73f4f2db1394cef8
Yevhen Piven <evgeniy13.91a@gmail.com>
2023-09-02 17:10:34 +0200

    change code from composite numbers to prime numbers

--- src/ua/com/yevhenpiven/Main.java
+++ src/ua/com/yevhenpiven/Main.java
@@ -1,22 +1,29 @@
 package ua.com.yevhenpiven;
 
-
+import java.util.LinkedList;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 	for(int i=2;i<=100;i++) {
-		boolean isTrue=true;
-		for(int j=2;j<i;j++) {
-			if(i%j==0)
-			isTrue=false;
-			break;
-		}
+		boolean isTrue=false;
+		LinkedList <Integer> list=new LinkedList<>();
+		for(int j=1;j<=i;j++) {
+			
+			if(i%j==0) {
+				
+			list.add(j);
+			//isTrue=false;
+			//break;
+		}}
+			if(list.size()==2)
+			isTrue=true;
+			
 		if (isTrue)
 			System.out.println(i);
-	}	
+	}	}}
 	
-	}
 	
-}
+	
+
